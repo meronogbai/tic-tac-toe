2fd2a4ca5dfb84db36f6c04468289132748c6ebf
Marco <marcoerhardt95@gmail.com>
2020-09-28 16:11:11 +0200

    add basic user interface to main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,37 @@
-#!/usr/bin/env ruby
-puts "Hello world!"
+
+#intro to the game
+
+puts "Welcome to Tic Tac Toe!"
+puts "Are you ready to play?"
+
+puts "How many players?"
+amount_players = gets.chomp
+
+puts "What is Player 1's name?"
+player1_name = gets.chomp
+
+puts "What is Player 2's name?"
+player2_name = gets.chomp
+
+puts "Input a number to choose a field!"
+
+#random player goes first
+
+puts "#{player1_name} goes first!"
+
+puts "#{player2_name} goes first!"
+
+#game starts
+
+chosen_field = gets.chomp.to_i
+
+puts "#{player_name} goes next!"
+
+puts "This field is already taken!"
+
+puts "Please input a valid number between 1 and 9!"
+
+puts "#{player_name} wins!"
+
+puts "It's a draw!"
+
