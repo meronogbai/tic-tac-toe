8668cb59d95a23465a80559bf663e6eabbdbac3f
Meron Ogbai <okbaymeron@gmail.com>
2020-09-29 15:18:31 +0300

    Finish display board

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -19,50 +19,57 @@ module Test
       'No Integer Error'
     end
   end
-
-
 end
 
 class Board
-  
+  def initialize
+    @first = ''
+    @second = ''
+    @third = ''
+    @fourth = ''
+    @fifth = ''
+    @sixth = ''
+    @seventh = ''
+    @eighth = ''
+    @ninth = ''
+  end
 
-  def fill_board(player, symbol, board)
-    player.each { |x|
-    case x
-    when 1
-      first = symbol
-    when 2
-      second = symbol
-    when 3
-      third = symbol
-    when 4
-      fourth = symbol
-    when 5
-      fifth = symbol
-    when 6
-      sixth = symbol
-    when 7
-      seventh = symbol
-    when 8
-      eighth = symbol
-    when 9
-      ninth = symbol
+  def fill_board(player, symbol)
+    player.each do |x|
+      case x
+      when 1
+        @first = symbol
+      when 2
+        @second = symbol
+      when 3
+        @third = symbol
+      when 4
+        @fourth = symbol
+      when 5
+        @fifth = symbol
+      when 6
+        @sixth = symbol
+      when 7
+        @seventh = symbol
+      when 8
+        @eighth = symbol
+      when 9
+        @ninth = symbol
+      end
     end
-  } 
   end
 
   def display_board(taken_fields)
+    first_player_moves, second_player_moves = taken_fields.partition.with_index { |_, i| i.even? }
 
-    board = "#{first} | #{second} | #{third}
-    #{fourth} | #{fifth} | #{sixth}
-    #{seventh} | #{eighth} | #{ninth}"
+    fill_board(first_player_moves, 'x')
+    fill_board(second_player_moves, 'o')
 
-    first_player_moves, second_player_moves = taken_fields.partition.with_index { |_,i| i.even? }
-
-    fill_board(first_player_moves, "x", board)
-    fill_board(second_player_moves, "o", board)
-
-    puts board
+    p "#{@first} | #{@second} | #{@third}"
+    p '-----------'
+    p "#{@fourth} | #{@fifth} | #{@sixth}"
+    p '-----------'
+    p "#{@seventh} | #{@eighth} | #{@ninth}"
   end
 end
 
