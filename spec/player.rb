38903e0efe89075f6b832d54a7a40516840347e0
Avijit <ajkacca@gmail.com>
2020-10-01 17:50:29 +0300

    method for name and random_player_start add

--- spec/player.rb
+++ spec/player.rb
@@ -6,4 +6,20 @@ describe Player do
     integer or empty or already taken."
   end
   player = Player.new(input, error)
+
+  describe '#name' do
+    it 'Checks if we can read name of player' do
+      expect(player.name).to eql 'Meron'
+    end
+    it 'Checks negative case' do
+      expect(player.name).not_to eql nil
+    end
+  end
+  describe '#random_player_start' do
+    input = proc { 'Avijit' }
+    second_player = Player.new(input, error)
+    it 'checks if rng works' do
+      expect(player.random_player_start(second_player) - [player.name, second_player.name]).to eql([])
+    end
+  end
 end
