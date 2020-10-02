3b81015d369b6588188015298d6f57de2cc74756
Meron Ogbai <okbaymeron@gmail.com>
2020-10-02 09:44:19 +0300

    Fix specs according to code review

--- spec/game_board_spec.rb
+++ spec/game_board_spec.rb
@@ -1,6 +1,19 @@
-require './lib/game.rb'
-require './lib/board.rb'
+require_relative '../lib/game.rb'
+require_relative '../lib/board.rb'
 describe Game do
+  describe '#taken_fields' do
+    game = Game.new
+    it 'Checks if we can read taken fields' do
+      expect(game.taken_fields).to eql []
+    end
+    it 'Checks if we can update taken fields' do
+      game.taken_fields=[1,2,3,4,5,6,8,7]
+      expect(game.taken_fields.push(9)).to eql [1,2,3,4,5,6,8,7,9]
+    end
+    it 'Test game draw' do
+      expect(game.taken_fields.length).to eql 9
+    end
+  end
   describe '#field_validation' do
     game = Game.new
     game.taken_fields = [1, 7, 2, 6, 3]
@@ -21,9 +34,13 @@ describe Game do
   end
   describe '#did_i_win' do
     game = Game.new
-    game.taken_fields = [1, 7, 2, 6, 3]
     it 'Checks if player 1 wins the game' do
+      game.taken_fields = [1, 7, 2, 6, 3]
       expect(game.did_i_win).to eql(1)
+    end
+    it 'Checks if player 1 wins the game' do
+      game.taken_fields = [7, 1, 6, 2, 5, 3]
+      expect(game.did_i_win).to eql(2)
     end
   end
   describe '#who_is_next' do
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,4 +1,4 @@
-require './lib/player.rb'
+require_relative '../lib/player.rb'
 describe Player do
   input = proc { 'Meron' }
   error = proc do
