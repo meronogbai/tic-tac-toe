63dc98ebe8bdc0e0bcb197825b06873ee119ec02
Marco <marcoerhardt95@gmail.com>
2020-10-07 17:38:11 +0200

    create first testing for display board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,7 +8,12 @@ describe Enumerable do
   describe '#display_board' do
     it 'updates and displays board' do
       game.taken_fields = [1, 7, 2, 6, 3]
-      expect(game.board.display_board(game)).to eql([9, 10, 11, 12])
+      expect(game.board.display_board(game)).to eql([
+        " x | x | x ", 
+        "-----------", 
+        "   |   | o ", 
+        "-----------", 
+        " o |   |   "])
     end
   end
 
