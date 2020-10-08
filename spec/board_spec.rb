a8a4ae748b12954a4e88edc73532f1262f82f335
Marco <marcoerhardt95@gmail.com>
2020-10-08 10:14:02 +0200

    finish testing for board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,14 +6,64 @@ describe Enumerable do
   let(:game) { Game.new }
 
   describe '#display_board' do
-    it 'updates and displays board' do
-      game.taken_fields = [1, 7, 2, 6, 3]
+    it 'splits taken fields and updates the board after every move' do
+      game.taken_fields = [1, 4, 2, 5, 3]
       expect(game.board.display_board(game)).to eql([
         " x | x | x ", 
         "-----------", 
-        "   |   | o ", 
+        " o | o |   ", 
         "-----------", 
-        " o |   |   "])
+        "   |   |   "])
+    end
+
+    it 'splits taken fields and updates the board after every move' do
+      game.taken_fields = [1, 4, 2, 5, 7, 6]
+      expect(game.board.display_board(game)).to eql([
+        " x | x |   ", 
+        "-----------", 
+        " o | o | o ", 
+        "-----------", 
+        " x |   |   "])
+    end
+
+    it 'splits taken fields and updates the board after every move' do
+      game.taken_fields = [1, 4, 7, 5, 2, 8, 3]
+      expect(game.board.display_board(game)).to eql([
+        " x | x | x ", 
+        "-----------", 
+        " o | o |   ", 
+        "-----------", 
+        " x | o |   "])
+    end
+
+    it 'splits taken fields and updates the board after every move' do
+      game.taken_fields = [1, 4, 2, 5, 7, 8, 6, 9, 3]
+      expect(game.board.display_board(game)).to eql([
+        " x | x | x ", 
+        "-----------", 
+        " o | o | x ", 
+        "-----------", 
+        " x | o | o "])
+    end
+
+    it 'splits taken fields and updates the board after every move' do
+      game.taken_fields = [1, 2, 3, 7, 4, 9, 6, 8]
+      expect(game.board.display_board(game)).to eql([
+        " x | o | x ", 
+        "-----------", 
+        " x |   | x ", 
+        "-----------", 
+        " o | o | o "])
+    end
+
+    it 'splits taken fields and updates the board after every move' do
+      game.taken_fields = [1, 5, 3, 2, 8, 4, 9, 6]
+      expect(game.board.display_board(game)).to eql([
+        " x | o | x ", 
+        "-----------", 
+        " o | o | o ", 
+        "-----------", 
+        "   | x | x "])
     end
   end
 
